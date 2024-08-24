46020604975c47fc0d903b97b8581a6fe72848b5
bychannel <1132431868@qq.com>
2024-08-24 14:30:39 +0800

    部署修改

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import DOCS from './help.html'
+
 addEventListener("fetch", (event) => {
   event.passThroughOnException();
   event.respondWith(handleRequest(event.request));
@@ -7,14 +9,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.bysimon.top": dockerHub,
+  "quay.bysimon.top": "https://quay.io",
+  "gcr.bysimon.top": "https://gcr.io",
+  "k8s-gcr.bysimon.top": "https://k8s.gcr.io",
+  "k8s.bysimon.top": "https://registry.k8s.io",
+  "ghcr.bysimon.top": "https://ghcr.io",
+  "cloudsmith.bysimon.top": "https://docker.cloudsmith.io",
+  "ecr.bysimon.top": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
@@ -43,6 +45,17 @@ async function handleRequest(request) {
       }
     );
   }
+
+  // return docs
+  if (url.pathname === "/") {
+    return new Response(DOCS, {
+      status: 200,
+      headers: {
+        "content-type": "text/html"
+      }
+    });
+  }
+
   const isDockerHub = upstream == dockerHub;
   const authorization = request.headers.get("Authorization");
   if (url.pathname == "/v2/") {
